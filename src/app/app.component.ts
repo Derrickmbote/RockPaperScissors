edfce5363216cd8b0c96a2f047baee0e64eecc6f
declanoluoch <45383997+declanoluoch@users.noreply.github.com>
2019-11-13 09:55:06 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,86 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'RockPaperScissors';
+  title ='Derrick Mbote';
+  UserSelected:string
+  CompSelected:string
+  compWeapon=["rock","paper", "scissors"]
+  UserScore=0
+  CompScore=0
+  action:string
+  status:string
+  UserPick (weapon:string){
+    this.UserSelected=weapon
+console.log('userselected',this.UserSelected)
+
+setTimeout(()=>{
+const randomNumber=Math.floor(Math.random()*3)
+this.CompSelected=this.compWeapon[randomNumber]
+console.log('comp selected',this.CompSelected)
+ this.checkResult()
+},1000)
+
+
+  }
+  clearfield(){
+    setTimeout(()=>{
+    this.status=''
+    this.UserSelected=''
+    this.CompSelected=''},1500)
+    
+  }
+  
+    win(user,comp){
+    this.UserScore++
+    console.log(user,comp)
+    this.UserSelected=user
+    this.CompSelected=comp
+    this.action="beats"
+    this.status="You win"
+    this.clearfield()
+
+  }
+  lose(user,comp){
+    this.CompScore++
+    console.log(user,comp)
+    this.UserSelected=user
+    this.CompSelected=comp
+    this.action="loses to"
+    this.status="You lose"
+    this.clearfield()
+}
+draw(user,comp){
+  console.log(user,comp)
+  this.UserSelected=user
+    this.CompSelected=comp
+    this.action="and"
+    this.status="You draw"
+    this.clearfield()
+
+}
+reset(){
+  this.UserScore=0
+  this.CompScore=0
+
+}
+  checkResult(){
+  const UserChoice=this.UserSelected 
+  const CompChoice=this.CompSelected
+  switch(UserChoice+CompChoice){
+    case "rockscissors":
+    case"scissorspaper":
+    case"paperrock":
+  this.win(UserChoice, CompChoice)
+    break;
+    case"scissorsrock":
+    case"rockpaper":
+    case"paperscissors":
+   this.lose(UserChoice, CompChoice)
+    break;
+    default:
+    this.draw(UserChoice, CompChoice)
+    break;
+  
+  }
+}
 }
